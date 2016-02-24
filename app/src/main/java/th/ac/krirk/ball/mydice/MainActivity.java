1f93b3e87715b61382ca4d66a00372e1017a9b41
ball <ballzacup1@gmail.com>
2016-02-23 19:40:45 -0800

    Bindwidget

--- app/src/main/java/th/ac/krirk/ball/mydice/MainActivity.java
+++ app/src/main/java/th/ac/krirk/ball/mydice/MainActivity.java
@@ -2,12 +2,27 @@ package th.ac.krirk.ball.mydice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView ImageView;
+    private Button backButton,RandomButton, nextButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        bindWidget();
+    }
+
+    private void bindWidget() {
+        ImageView = (android.widget.ImageView) findViewById(R.id.imageView);
+        backButton = (Button) findViewById(R.id.button);
+        RandomButton = (Button) findViewById(R.id.button3);
+        nextButton = (Button) findViewById(R.id.Random);
     }
 }
